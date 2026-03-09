c80b46581951cd7dff9eeaf3c5e7ffe97a2f8da4
evaamarina <evitamp410@gmail.com>
2026-03-09 13:29:05 +0100

    Parcial EMP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { searchCocktailByName, getRandomCocktail } from "./lib/api/cocktail";
 import type { Cocktail } from "./types";
 import { useRouter } from "next/navigation";
 import CocktailCard from "./components/Cocktail";
 
 const Home = () => {
+
   const [search, setSearch] = useState("");
   const [cocktails, setCocktails] = useState<Cocktail[]>([]);
   const router = useRouter();
-
-  useEffect(() => {
-    searchCocktailByName("margarita").then((data) => {
-      setCocktails(data || []);
-    });
-  }, []);
 
   const handleSearch = async () => {
     const data = await searchCocktailByName(search);
@@ -30,25 +25,39 @@ const Home = () => {
   return (
     <div className="container">
       <h1>Buscador de Cocktails</h1>
+
       <div className="searchContainer">
-        <input type="text"
+
+        <input
+          type="text"
           placeholder="Buscar cocktail..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button onClick={handleSearch}>Buscar</button>
+
+        <button onClick={handleSearch}>
+          Buscar
+        </button>
+
       </div>
 
-      <button onClick={handleRandom}>Dime algo bonito</button>
-      
-      <div className="cocktailGrid">
-        {cocktails.map((cocktail) => (
-          <CocktailCard
-            key={cocktail.idDrink}
-            cocktail={cocktail}
-          />
-        ))}
-      </div>
+      <button onClick={handleRandom}>
+        Dime algo bonito
+      </button>
+
+      {cocktails.length > 0 && (
+        <div className="cocktailGrid">
+
+          {cocktails.map((cocktail) => (
+            <CocktailCard
+              key={cocktail.idDrink}
+              cocktail={cocktail}
+            />
+          ))}
+
+        </div>
+      )}
+
     </div>
   );
 };
